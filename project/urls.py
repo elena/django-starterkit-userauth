83ec343c3a11694c1369089804cbeb5c4410542e
Elena Williams <elena.williams@thecut.net.au>
2013-11-14 13:14:22 +0800

    Adding (plugging in) accounts and home urls.

--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import patterns, include, url
+from .views import HomePageView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', HomePageView.as_view(), name='home'),
+
+    ## User login.
+    url(r'^user/', include('accounts.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
